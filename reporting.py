966f0abf9c3e0ce7453bbbd9a04009192487e31e
adonegan <48014273+adonegan@users.noreply.github.com>
2021-04-05 23:30:53 +0100

    Add parent class to remove duplicate code

--- reporting.py
+++ reporting.py
@@ -1,25 +1,19 @@
-class AccountingReport:
+class Report:
     def __init__(self, emp_list):
         self._emp_list = emp_list
 
+class AccountingReport(Report):
     def print_accounting_report(self):
         print("Accounting")
         print("==========")
         for e in self._emp_list:
             print(f"{e.get_full_name()}, ${e.salary}")
 
-
-class StaffingReport:
-    def __init__(self, emp_list):
-        self._emp_list = emp_list
-
+class StaffingReport(Report):
     def print_staffing_report(self):
         print("Staffing")
         print("========")
         for e in self._emp_list:
             print(f"{e.get_full_name()}, {e .job_title}")
 
-
-# Notes: why doesn't just importing from employee.py work? Why is def __init__ and self._emp_list needed for this one?
-
-# Look into 'dependency injection' and 'decoupling'
+# Create super/parent class to remove duplicate code. This class is abstract and is not supposed to be instantiated. 
