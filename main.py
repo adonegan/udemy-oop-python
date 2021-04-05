969007f1c407c8949e57dcd99ecab191cdb55d39
adonegan <48014273+adonegan@users.noreply.github.com>
2021-04-05 19:50:53 +0100

    Add subclasses for job titles

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 # Code with subclasses
+# Using inheritance to prevent duplicate code
 
 class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
 
+class Mechanic(Employee):
+    # class variable
+    # use underscore
+    job_title = "Mechanic"
+
+class Cook(Employee):
+    job_title = "Cook"
+
+class Attendant(Employee):
+    job_title = "Station Attendant"
+
+class Manager(Employee):
+    job_title = "Manager"
+
 employees = [
-    Employee("Vera", 2000),
-    Employee("Chuck", 1800),
-    Employee("Samantha", 1800),
-    Employee("Roberto", 2100),
-    Employee("Joe", 2000),
-    Employee("Dave", 2200),
-    Employee("Tina", 2300)
+    Manager("Vera", 2000),
+    Attendant("Chuck", 1800),
+    Attendant("Samantha", 1800),
+    Cook("Roberto", 2100),
+    Mechanic("Joe", 2000),
+    Mechanic("Dave", 2200),
+    Mechanic("Tina", 2300)
 ]
+
+for e in employees:
+    print(f"{e.name}, ${e.salary}, {e.job_title}")
+
+# no need to write the dunder method in sub / child classes, because they inherit it from super / parent class
