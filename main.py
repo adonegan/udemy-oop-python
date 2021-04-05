7d317a4a212944b2560a0101b01e14f21f3ec41a
adonegan <48014273+adonegan@users.noreply.github.com>
2021-04-06 00:20:44 +0100

    Add imports and shift methods

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ from employee import Cook
 from employee import Mechanic
 from reporting import AccountingReport
 from reporting import StaffingReport
+from reporting import ScheduleReport
+from shift import MorningShift
+from shift import AfternoonShift
+from shift import NightShift
 
 employees = [
-    Manager("Vera", "Schmidt", 2000),
-    Attendant("Chuck", "Norris", 1800),
-    Attendant("Samantha", "Carrington", 1800),
-    Cook("Roberto", "Jacketti", 2100),
-    Mechanic("Joe", "Rama", 2000),
-    Mechanic("Dave", "Dreilig", 2200),
-    Mechanic("Tina", "River", 2300),
-    Mechanic("Chuck", "Rainey", 1800)
+    Manager("Vera", "Schmidt", 2000, MorningShift()),
+    Attendant("Chuck", "Norris", 1800, MorningShift()),
+    Attendant("Samantha", "Carrington", 1800, AfternoonShift()),
+    Cook("Roberto", "Jacketti", 2100, MorningShift()),
+    Mechanic("Joe", "Rama", 2000, MorningShift()),
+    Mechanic("Dave", "Dreilig", 2200, MorningShift()),
+    Mechanic("Tina", "River", 2300, AfternoonShift()),
+    Mechanic("Chuck", "Rainey", 1800, NightShift()),
 ]
 
 reports = [
     AccountingReport(employees),
-    StaffingReport(employees)
+    StaffingReport(employees),
+    ScheduleReport(employees), 
 ]
 
 for r in reports:
